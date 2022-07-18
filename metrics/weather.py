afb5f98ec63ce97d045ae38343706dcd26035ddb
PlanetTeamSpeakk <PlanetTeamSpeakk@users.noreply.github.com>
2022-07-19 01:05:23 +0200

    Add weather metric

---
+++ metrics/weather.py
@@ -0,0 +1,18 @@
+import requests
+from datetime import datetime
+from __main__ import creds
+
+def setup(register, db):
+    register("weather", 30 * 60, 5 * 60, read)
+
+def read(c):
+    weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={creds.LAT}&lon={creds.LON}&appid={creds.OWM_API_KEY}&units=metric").json()
+    main = weather["main"]
+    c.execute("INSERT INTO weather (description, temp, feels_like, temp_min, temp_max, pressure, humidity, wind_speed, wind_angle, clouds, sunrise, sunset) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", 
+        (weather["weather"][0]["description"],
+        main["temp"], main["feels_like"], main["temp_min"], main["temp_max"], main["pressure"], main["humidity"],
+        weather["wind"]["speed"], weather["wind"]["deg"],
+        weather["clouds"]["all"],
+        datetime.utcfromtimestamp(weather["sys"]["sunrise"]), datetime.utcfromtimestamp(weather["sys"]["sunset"])))
+
+    return weather

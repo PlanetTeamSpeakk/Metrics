ca8cb9427bfec737badea0ef542f247b1e9caa72
PlanetTeamSpeakk <PlanetTeamSpeakk@users.noreply.github.com>
2022-07-19 17:15:30 +0200

    Merge temp and humi metrics

--- metrics/humitemp.py
+++ metrics/humitemp.py
@@ -3,20 +3,13 @@ import board
 import json
 
 dht = adafruit_dht.DHT22(board.D18)
-temp = 0
 
 def setup(register, db):
-    register("temperature", 30, 5, read_temp)
-    register("humidity", 30, 5, read_humi)
+    register("humitemp", 30, 5, read)
 
-def read_temp(c):
-    global temp
+def read(c):
     temp = dht.temperature
-
-    return temp
-
-def read_humi(c):
     humi = dht.humidity
     c.execute("INSERT INTO humitemp (temperature, humidity) VALUES (%s, %s);", (temp, humi))
 
-    return humi
+    return {"temp": temp, "humi": humi}

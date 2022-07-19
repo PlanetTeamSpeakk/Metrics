d575d5de36ada105e2fe251cc2e4d97dbf1ad026
PlanetTeamSpeakk <PlanetTeamSpeakk@users.noreply.github.com>
2022-07-19 16:55:32 +0200

    Make pi temp less fluctuent

--- metrics/pi-temp.py
+++ metrics/pi-temp.py
@@ -4,7 +4,8 @@ def setup(register, db):
     register("pi-temp", 30, 5, read)
 
 def read(c):
-    pi_temp = float(subprocess.check_output("/opt/vc/bin/vcgencmd measure_temp", shell=True)[5:-3])
+    i = 5
+    pi_temp = sum(float(subprocess.check_output("/opt/vc/bin/vcgencmd measure_temp", shell=True)[5:-3]) for x in range(i)) / i
     c.execute("INSERT INTO pi_temp (temp) VALUES (%s);", (pi_temp,))
 
     return pi_temp
